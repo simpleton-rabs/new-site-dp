d10c4d6a1d2eb2efc17ad65d5dfd913c48420b94
simpleton rabs <74567810+simpleton-rabs@users.noreply.github.com>
2025-05-06 22:27:40 +0100

    KMLMKL

--- js/js.js
+++ js/js.js
@@ -1,18 +1,18 @@
 let something = document.getElementById("introimg");
 console.log("The variable something is linking to ", something);
 
-const earthHum = new Audio("../sound/earthhum.mp3");
+const soundOne = new Audio("../sounds/sound1.mp3");
 // console.log("The variable earthHum is linking to ", earthHum);
 
 const click = document.getElementById("click");
 // console.log("Clicketh ", click);
 
 click.onclick = () => {
-    if (earthHum.paused){
+    if (soundOne.paused){
         click.innerHTML = "Click me to pause";
-        earthHum.play();
+        soundOne.play();
     } else {
         click.innerHTML = "Click me to play";
-        earthHum.pause();
+        soundOne.pause();
     }
 }
