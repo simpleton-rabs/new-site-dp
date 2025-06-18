6046aaee3fa733384568387fb9da4e83bb7db8e7
simpleton rabs <74567810+simpleton-rabs@users.noreply.github.com>
2025-06-18 20:32:00 +0100

    kjljk

--- js/js.js
+++ js/js.js
@@ -4,15 +4,36 @@ console.log("The variable something is linking to ", something);
 const soundOne = new Audio("../sounds/sound1.mp3");
 // console.log("The variable earthHum is linking to ", earthHum);
 
+soundOne.loop = true;
+
 const click = document.getElementById("click");
 // console.log("Clicketh ", click);
 
-click.onclick = () => {
+const speakerIcon = document.getElementById("speaker");
+// console.log("Link to speaker ", speakerIcon);
+
+// console.log("Link to speaker ", speakerIcon.src);
+
+
+// click.onclick = () => {
+//     if (soundOne.paused){
+//         click.innerHTML = "Click me to pause";
+//         soundOne.play();
+//     } else {
+//         click.innerHTML = "Click me to play";
+//         soundOne.pause();
+//     }
+// }
+
+speakerIcon.onclick = () => {
     if (soundOne.paused){
-        click.innerHTML = "Click me to pause";
+        // speakerIcon.src = "/menuimg/speakeroff.png"; 
+
+        speakerIcon.src = "../menuimg/speakeroff.png";
         soundOne.play();
+        
     } else {
-        click.innerHTML = "Click me to play";
+        speakerIcon.src = "../menuimg/speakeron.png";
         soundOne.pause();
     }
 }
