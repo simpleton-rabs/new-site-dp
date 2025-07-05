62a7d6c7d186ef29e38bbeae338c5761373bb12f
simpleton rabs <74567810+simpleton-rabs@users.noreply.github.com>
2025-07-05 23:55:59 +0100

    dfdsfds

--- js/js.js
+++ js/js.js
@@ -1,5 +1,5 @@
-let something = document.getElementById("introimg");
-console.log("The variable something is linking to ", something);
+// let something = document.getElementById("introimg");
+// console.log("The variable something is linking to ", something);
 
 const soundOne = new Audio("../sounds/sound1.mp3");
 // console.log("The variable earthHum is linking to ", earthHum);
@@ -10,6 +10,21 @@ const click = document.getElementById("click");
 // console.log("Clicketh ", click);
 
 const speakerIcon = document.getElementById("speaker");
+
+const greeTing = document.getElementById("greeting");
+
+console.log("link to greets", greeTing.innerHTML);
+
+var today = new Date();
+
+console.log("link to date", today);
+
+var currentHour = today.getHours();
+
+// console.log("link to hour", currentHour);
+
+// var currentHour = 0;
+
 // console.log("Link to speaker ", speakerIcon);
 
 // console.log("Link to speaker ", speakerIcon.src);
@@ -36,4 +51,14 @@ speakerIcon.onclick = () => {
         speakerIcon.src = "../menuimg/speakeron.png";
         soundOne.pause();
     }
+}
+
+if (currentHour > 17) {
+    greeTing.innerHTML = "Good Evening!!!"
+} else if (currentHour > 12) {
+    greeTing.innerHTML = "Good Afternoon!!!";
+} else if (currentHour > 0){
+    greeTing.innerHTML = "Good Morning!!!";
+} else {
+    greeTing.innerHTML = "Welcome!!!"
 }
