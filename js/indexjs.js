a75382fa05d96fa0bb7ac810ac146376a9325593
simpleton rabs <74567810+simpleton-rabs@users.noreply.github.com>
2025-07-06 22:02:28 +0100

    retrre

--- js/indexjs.js
+++ js/indexjs.js
@@ -1,64 +1,33 @@
-// let something = document.getElementById("introimg");
-// console.log("The variable something is linking to ", something);
 
+// create audio object
 const soundOne = new Audio("../sounds/sound1.mp3");
-// console.log("The variable earthHum is linking to ", earthHum);
-
 soundOne.loop = true;
-
-const click = document.getElementById("click");
-// console.log("Clicketh ", click);
-
+// const for speaker icon
 const speakerIcon = document.getElementById("speaker");
-
+// const for greeting
 const greeTing = document.getElementById("greeting");
-
-console.log("link to greets", greeTing.innerHTML);
-
+//var for date and time
 var today = new Date();
-
-console.log("link to date", today);
-
 var currentHour = today.getHours();
-
-// console.log("link to hour", currentHour);
-
-// var currentHour = 0;
-
-// console.log("Link to speaker ", speakerIcon);
-
-// console.log("Link to speaker ", speakerIcon.src);
-
-
-// click.onclick = () => {
-//     if (soundOne.paused){
-//         click.innerHTML = "Click me to pause";
-//         soundOne.play();
-//     } else {
-//         click.innerHTML = "Click me to play";
-//         soundOne.pause();
-//     }
-// }
-
+//event handler for if speaker icon is clicked
 speakerIcon.onclick = () => {
+//if sound paused change icon to speaker off and play sound
     if (soundOne.paused){
-        // speakerIcon.src = "/menuimg/speakeroff.png"; 
-
         speakerIcon.src = "../menuimg/speakeroff.png";
         soundOne.play();
-        
+//else sound must be playing so change icon to speaker on and pause sound        
     } else {
         speakerIcon.src = "../menuimg/speakeron.png";
         soundOne.pause();
     }
 }
-
-if (currentHour > 17) {
-    greeTing.innerHTML = "Good Evening!!!"
-} else if (currentHour > 12) {
-    greeTing.innerHTML = "Good Afternoon!!!";
-} else if (currentHour > 0){
-    greeTing.innerHTML = "Good Morning!!!";
+//set greeting as per current hour
+if (currentHour >= 17) {
+    greeTing.innerHTML = "Good Evening"
+} else if (currentHour >= 12) {
+    greeTing.innerHTML = "Good Afternoon";
+} else if (currentHour >= 0){
+    greeTing.innerHTML = "Good Morning";
 } else {
-    greeTing.innerHTML = "Welcome!!!"
+    greeTing.innerHTML = "Welcome"
 }
